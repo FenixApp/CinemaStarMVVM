0ab085c37eb42efb41e47de9732433c3809d7fce
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-04-25 00:37:21 +0500

    Сделан UI первого экрана

---
+++ CinemaStar/FilmsModule/Model/Film.swift
@@ -0,0 +1,12 @@
+// Film.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+/// Данные фильма
+struct Film {
+//    let posterURL: URL
+    let name: String
+    let rating: Double
+    let id: Int
+}
